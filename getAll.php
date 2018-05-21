52437c1114ab8555e342ff581491337085488c7a
Ndeye Sokhna Seck <ndeye-sokhna.seck@etu.unice.fr>
2018-05-21 10:31:06 +0200

    some changes

--- getAll.php
+++ getAll.php
@@ -2,30 +2,22 @@
 
  require "connexion.php";
 
- $username = $_POST["username"];
-
 mysqli_set_charset($connexion, 'utf8'); 
 
-//In order to get incidents declared by user only
-
-$mysql_query = "SELECT ut.surname, ut.name, inc.id_incident,  inc.title, inc.content, inc.location, inc.date, t.value as tag, imp.value as importance, urg.value as urgence from incident as inc,
-  default_tag as t, importance as imp, urgence as urg, user_table as ut WHERE
-      ut.username like '$username' and
-    t.id_tag = inc.id_tag and urg.id_urgence = inc.id_urgence and imp.id_importance = inc.id_importance and inc.id_incident
-IN (SELECT ia.id_incident from incident_author as ia where ia.id_user IN (SELECT us.id_user from user_table as us
-    WHERE us.username like '$username')) ORDER BY inc.date DESC;";
+$mysql_query = "SELECT u.surname, u.name, inc.id_incident, inc.title, inc.content, inc.location, inc.date, t.value as tag, imp.value as importance, urg.value as urgence from incident as inc, default_tag as t, importance as imp, urgence as urg, user_table as u WHERE inc.id_tag = t.id_tag and inc.id_importance = imp.id_importance and inc.id_urgence = urg.id_urgence 
+		and (u.surname, u.name) IN (SELECT us.surname, us.name FROM user_table as us WHERE us.id_user = (SELECT ia.id_user FROM incident_author as ia WHERE ia.id_incident = inc.id_incident)) ORDER BY inc.date desc ";
 
 $result = mysqli_query($connexion, $mysql_query);
 $rows = array();
 
 if(mysqli_num_rows($result) > 0){
-  while($row = $result->fetch_assoc()) {
+	while($row = $result->fetch_assoc()) {
         $rows[] = $row;
     }
     print json_encode($rows, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
 }else{
-  echo "fail";
+	 echo "fail";
  }
 
 $connexion->close();
--- getIncidentsByUser.php
+++ getIncidentsByUser.php
@@ -8,7 +8,7 @@ mysqli_set_charset($connexion, 'utf8');
 
 //In order to get incidents declared by user only
 
-$mysql_query = "SELECT ut.surname, ut.name, inc.title, inc.content, inc.location, inc.date, t.value as tag, imp.value as importance, urg.value as urgence from incident as inc,
+$mysql_query = "SELECT ut.surname, ut.name, inc.id_incident,  inc.title, inc.content, inc.location, inc.date, t.value as tag, imp.value as importance, urg.value as urgence from incident as inc,
   default_tag as t, importance as imp, urgence as urg, user_table as ut WHERE
       ut.username like '$username' and
     t.id_tag = inc.id_tag and urg.id_urgence = inc.id_urgence and imp.id_importance = inc.id_importance and inc.id_incident
